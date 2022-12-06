94b013e7c381a4d6fa08035e0167fe77b02dc85e
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-06 20:07:59 +0300

    ADD dark mode config

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const theme = localStorage.getItem("theme")
+
+if(theme === "dark" || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add("dark")
+} else {
+  document.documentElement.classList.add("light")
+} 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
