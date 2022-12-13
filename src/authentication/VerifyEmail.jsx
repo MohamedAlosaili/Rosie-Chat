6dbce48f50cb480807d6be5af439225da5ee18d1
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-13 15:38:42 +0300

    REFACTOR email-sent & timer

--- src/authentication/VerifyEmail.jsx
+++ src/authentication/VerifyEmail.jsx
@@ -16,9 +16,13 @@ const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
     const [sendEmailVerification, sending, error] = useSendEmailVerification(auth)
     const [timer, setTimer] = useState(() => {
         const sessionTimer = JSON.parse(sessionStorage.getItem("timer"))
-        if(sessionTimer && +sessionTimer.minutes !== 0 && +sessionTimer.seconds !== 0) return sessionTimer
+        const verify = JSON.parse(sessionStorage.getItem("email-verify"))
+        
+        if(sessionTimer && !sessionTimer.isFinished && verify?.email === user.email) {
+            return sessionTimer
+        }
 
-        const newTimer = { minutes: "01", seconds: "59", isFinished: false }
+        const newTimer = { minutes: "01", seconds: "00", isFinished: false }
         return newTimer
     })
 
@@ -26,11 +30,11 @@ const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
     useEffect(() => {
         interval = setInterval(countDown, 1000)
         
-        const emailSent = JSON.parse(sessionStorage.getItem("email-sent"))
+        const verify = JSON.parse(sessionStorage.getItem("email-verify"))
         
-        if(!emailSent) {
+        if(!verify?.isSend || verify?.email !== user.email) {
             sendVerificationEmail()
-            sessionStorage.setItem("email-sent", true)
+            sessionStorage.setItem("email-verify", JSON.stringify({isSend: true, email: user.email}))
         }
         
         return () => clearInterval(interval)
@@ -39,12 +43,12 @@ const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
     // store the remaining timer in sessionStorage if the component unmount
     useEffect(() => () => sessionStorage.setItem("timer", JSON.stringify(timer)), [timer])
 
-    // change the emailSent state depending on the timer or if rsend clicked
+    // change the email-verify state depending on the timer or if rsend clicked
     useEffect(() => {
-        timer.isFinished && sessionStorage.setItem("email-sent", false)
+        timer.isFinished && sessionStorage.setItem("email-verify", JSON.stringify({isSend: false, email: user.email}))
 
         if(+timer.seconds > 0 && !timer.isFinished) 
-            sessionStorage.setItem("email-sent", true)
+            sessionStorage.setItem("email-verify", JSON.stringify({isSend: true, email: user.email}))
     }, [timer.isFinished])
 
     async function sendVerificationEmail() {
@@ -52,7 +56,7 @@ const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
             interval = setInterval(countDown, 1000)
             setTimer({
                 minutes: "01", 
-                seconds: "59",
+                seconds: "00",
                 isFinished: false
             })
         }
@@ -61,16 +65,12 @@ const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
     
     function countDown() {
         setTimer(({minutes, seconds, isFinished}) => {
-            let newMin = +minutes
-            let newSec = seconds - 1
+            let newMin = 0
+            let newSec = +seconds === 0 ? 59 : +seconds - 1
 
             if(+minutes === 0 && +seconds === 0) {
                 clearInterval(interval); 
                 return  { minutes: `00`, seconds: `00`, isFinished: true} 
-            }
-            if(newSec < 0) {
-                newMin = 0
-                newSec = 59
             }
 
             return {
@@ -101,7 +101,7 @@ const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
                     disabled={!timer.isFinished}
                     className={
                         `bg-accent p-2 rounded-xl ${timer.isFinished && "active:scale-[0.98] hover:bg-accent-600"}
-                         ${!timer.isFinished && "cursor-not-allowed"} text-sm font-medium flex-1 text-primary-200`
+                         ${!timer.isFinished && "cursor-not-allowed opacity-30"} text-sm font-medium flex-1 text-primary-200`
                     }
                 >
                     Resend Email
