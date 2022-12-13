f878f595dee8ff35a3576cdfa42600802b54bc8d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-13 18:09:53 +0300

    UPDATE disabled prop & REFACTOR style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,21 @@
 import { memo } from "react"
 import PropTypes from "prop-types"
 
-const Button = ({children, handleClick, bg, padding, additionClasses}) => {
+const Button = ({children, handleClick, bg, padding, additionClasses, disabled}) => {
 
 
     const bgColor = 
         (bg === "full") ? 
-        `bg-accent hover:bg-accent-600 ring-accent hover:ring-accent-600` : 
+        `bg-accent ${!disabled && "hover:bg-accent-600"} ring-accent hover:ring-accent-600` : 
         `ring-slate-400 dark:ring-slate-700 hover:ring-slate-500 hover:bg-primary-100 dark:hover:bg-primary-800`
 
     return (
         <button 
+            disabled={disabled}
             onClick={handleClick}
             className={
-                `flex gap-3 items-center justify-center rounded-xl active:scale-[0.98] text-primary-200 font-medium
-                ring-1 ${bgColor} ${padding} ${additionClasses}
+                `flex gap-3 items-center justify-center rounded-xl text-primary-200 font-medium 
+                ring-1 ${bgColor} ${padding} ${!disabled && "active:scale-[0.98]"} ${additionClasses}
                 `
             }
         >
@@ -23,18 +24,20 @@ const Button = ({children, handleClick, bg, padding, additionClasses}) => {
     )
 }
 
+Button.propTypes = {
+    children: PropTypes.oneOf([PropTypes.string, PropTypes.element]).isRequired,
+    handleClick: PropTypes.func,
+    bg: PropTypes.string,
+    padding: PropTypes.string,
+    additionClasses: PropTypes.string,
+    disabled: PropTypes.bool
+}
+
 Button.defaultProps = {
     bg: "full",
     padding: "p-2 mb-6" ,
-    additionClasses: "" 
-}
-
-Button.propTypes = {
-    children: PropTypes.oneOf([PropTypes.string, PropTypes.element]).isRequired,
-    handleClick: PropTypes.func.isRequired,
-    bg: PropTypes.string,
-    padding: PropTypes.string,
-    additionClasses: PropTypes.string
+    additionClasses: "",
+    disabled: false 
 }
 
 export default memo(Button)
