c487c57961e62d75b3f3f752a9e190874e08b9fa
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-15 09:41:53 +0300

    UPDATE config vite to use absolute path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "./src/components"),
+      features: path.resolve(__dirname, "./src/features"),
+      "rosie-firebase": path.resolve(__dirname, "./src/rosie-firebase"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      imgs: path.resolve(__dirname, "./src/imgs"),
+    },
+  },
+  plugins: [react()],
+});
