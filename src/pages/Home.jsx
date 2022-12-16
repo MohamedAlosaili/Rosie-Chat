e94a167e5a844f3146e9f96528e307a38f1f80c9
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-16 14:58:38 +0300

    REFACTOR style & ADD components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,12 @@
-import { useSendEmailVerification } from "react-firebase-hooks/auth";
+import { SideBox } from "features/sidebox"
+import { ChatBox } from "features/chatbox"
 
-import { SignOut } from "features/authentication";
-import { auth } from "rosie-firebase";
 
 export default function Home() {
-  const [sendEmailVerification, sending, error] =
-    useSendEmailVerification(auth);
-
-  async function verfiyEmail() {
-    try {
-      const sendMessage = await sendEmailVerification(auth.currentUser);
-      console.log("No Errors");
-      console.log(sendMessage);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  console.log("sending, error");
-  console.log(sending, error);
-  console.log(auth.currentUser);
   return (
-    <div className="h-100">
-      <h1>Home live now</h1>
-      <button onClick={verfiyEmail}>Verify my email</button>
-      <SignOut />
+    <div className="absolute inset-0 overflow-hidden flex">
+      <SideBox />
+      <ChatBox />
     </div>
   );
 }
