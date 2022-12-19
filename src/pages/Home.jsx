9dac4be1004aae9f8ddd82a067f6a93c29c5343a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-19 15:53:48 +0300

    UPDATE initiate local state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
+
 import { SideBox } from "features/sidebox"
 import { ChatBox } from "features/chatbox"
 
-
 export default function Home() {
+
+  const [chatId, setChatId] = useState("")
+
   return (
     <div className="absolute inset-0 overflow-hidden flex">
-      <SideBox />
-      <ChatBox />
+      <SideBox setChatId={setChatId} />
+      <ChatBox chatId={chatId} setChatId={setChatId} />
     </div>
   );
 }
