863cbd16863867a01fb2888707649cc7cf7f3e06
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-19 14:39:59 +0300

    UPDATE uncomment ternary condition

--- src/features/chatbox/ChatBox.jsx
+++ src/features/chatbox/ChatBox.jsx
@@ -4,9 +4,9 @@ function ChatBox({ chatId }) {
   return (
     <div className="basis-[30rem] grow shrink">
       {
-        // chatId ?
-        <Conversation /> 
-        // : <h1>Select chat to start messaging</h1>
+        chatId 
+        ? <Conversation /> 
+        : <h1>Select chat to start messaging</h1>
       }
     </div>
   )
