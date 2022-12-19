e7016dd0dac4b17230809e46087395fcd660b48f
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-19 16:16:02 +0300

    UPDATE reduce files

--- src/rosie-firebase/index.js
+++ src/rosie-firebase/index.js
@@ -1,13 +1,35 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+} from "firebase/auth";
+import { getFirestore, collection } from "firebase/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 import firebaseConfig from "../firebaseConfig";
-
-import { signWithProviders } from "./authWithProviders";
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 
-export { auth, db, signWithProviders };
+export async function signWithProviders(providerType = "google") {
+  const windowWidth = window.innerWidth;
+
+  const providers = {
+    google: new GoogleAuthProvider(),
+  };
+
+  const provider = providers[providerType];
+  const signMethod = windowWidth <= 768 ? signInWithRedirect : signInWithPopup;
+  try {
+    await signMethod(auth, provider);
+  } catch (error) {
+    return { type: "error", ...error };
+  }
+}
+
+export { auth, db, collection, useAuthState, useCollectionData };
