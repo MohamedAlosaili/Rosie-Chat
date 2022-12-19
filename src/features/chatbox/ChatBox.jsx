96ab7ba2fa5675b4510c88013e49a19437ffcfc1
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-19 16:12:30 +0300

    UPDATE create empty state

--- src/features/chatbox/ChatBox.jsx
+++ src/features/chatbox/ChatBox.jsx
@@ -1,12 +1,24 @@
 import Conversation from "./Conversation"
+import { selectChat } from "imgs"
 
 function ChatBox({ chatId }) {
   return (
     <div className="basis-[30rem] grow shrink">
       {
         chatId 
-        ? <Conversation /> 
-        : <h1>Select chat to start messaging</h1>
+        ? (
+          <Conversation chatId={chatId} />
+        ) 
+        : (
+          <div className="h-full flex flex-col justify-center items-center">
+            <img 
+              src={selectChat} 
+              alt="empty chat" 
+              className="w-full max-w-lg"
+            />
+            <h3 className="text-lg font-medium mt-8 text-primary-300">Select a chat to start messaging</h3>
+          </div>
+        )
       }
     </div>
   )
