58b75d07dbed79b77ee094d166ac051c053865c6
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-20 17:25:37 +0300

    REFACTOR listen for selectedChat document

--- src/features/chatbox/Conversation.jsx
+++ src/features/chatbox/Conversation.jsx
@@ -1,24 +1,43 @@
 import PropTypes from "prop-types"
 
+import { collection, query, orderBy } from "firebase/firestore"
+import { useCollectionData } from "react-firebase-hooks/firestore"
+
+import { db } from "rosie-firebase"
 import { defaultAvatar } from "imgs" 
 import Message from "./Message"
 import Form from "./Form"
+import { StatusMessage } from "../../components"
 
-function Conversation() {
+function Conversation({ selectedChat }) {
+
+    const q = query(
+        collection(db, `${selectedChat.isGroup ? "groups" : "direct"}/${selectedChat.id}/messages`), 
+        orderBy("createdAt")
+    )
+
+    const [messages, loading, error] = useCollectionData(q)    
+
     return (
-        <div className="h-full flex flex-col bg-[url('/src/imgs/conversation/chat-bg.png')] bg-contain">
+        <div className="h-full flex flex-col bg-[url('/src/imgs/chat/chat-bg.png')] bg-contain">
+            { loading && <StatusMessage message="Loading..." type="loading" /> }
+            { error && <StatusMessage message={error?.code} type="error" /> }
             <header className="flex items-center gap-4 p-4 pt-6 border-b border-primary-800 dark:bg-primary-900">
                 <img 
-                    src="https://lh3.googleusercontent.com/a/AEdFTp4ump6-jkdHxj4z110nZxqaZ-TVTN-2TdlVU3919w=s96-c" 
-                    alt={`user photo`} 
+                    src={selectedChat.photoURL} 
+                    alt={`${selectedChat.name} photo`} 
                     className="h-10 aspect-square object-cover rounded-50"
                     onError={(e) => e.target.src = defaultAvatar}
                 />
-                <h3 className="font-medium dark:text-primary-200">Mohamed</h3>
+                <h3 className="font-medium dark:text-primary-200">{ selectedChat.name }</h3>
             </header>
             <main className="flex-1 overflow-y-auto overflow-x-hidden p-4 scrollbar">
                 <div className="max-w-2xl mx-auto">
-                    
+                    {
+                        messages?.map(msg => (
+                            <Message key={msg} {...msg} isGroup={selectedChat.isGroup} />
+                        ))
+                    }
                 </div>
             </main>
             <footer className="w-full max-w-2xl mx-auto p-2 py-3 border-t dark:border-primary-800">
