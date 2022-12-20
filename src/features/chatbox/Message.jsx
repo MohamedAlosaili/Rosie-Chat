4f7036cc33acdd772de093732982fee093ea8382
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-20 22:06:36 +0300

    UPDATE convert timestamp to readable time

--- src/features/chatbox/Message.jsx
+++ src/features/chatbox/Message.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { auth } from "rosie-firebase"
 import { defaultAvatar } from "imgs"
 
-const Message = ({ type, uid, message, time, photoURL, isGroup }) => {
+const Message = ({ type, uid, message, createdAt, photoURL, isGroup }) => {
 
     if(type === "announce") {
         return (
@@ -12,15 +12,27 @@ const Message = ({ type, uid, message, time, photoURL, isGroup }) => {
             </div>
         )
     }
+    // Convert firebase timestamp into readable time
+    let messageTime
+    if(createdAt) {
+        const date = `${createdAt.toDate()}`
+        const extractedTime = date.match(/\d\d:\d\d/g)[0].split(":")
+        
+        const period = +extractedTime[0] >= "12" ? "PM" : "AM"
+        const minutes = extractedTime[1]
+        const hours = extractedTime[0] % 12 || 12
+
+        messageTime = `${hours}:${minutes} ${period}`
+    } else { console.log("error", type, uid, message, createdAt, photoURL, isGroup) }
 
     const userMsg = auth.currentUser.uid === uid
-
+    console.log(userMsg)
     return (
         <div className={`flex justify-start gap-2 ${userMsg ? "flex-row-reverse" : ""} my-2`}>
             {isGroup && <img src={photoURL} alt="user message" className="w-8 h-8 object-cover" />}
             <div className={`max-w-[70%] py-2 px-4 rounded-xl dark:bg-${userMsg ? "accent" : "primary-800"} dark:text-primary-200`}>
               <p>{message}</p>
-              <time className={`block text-${userMsg ? "left" : "right"} text-xs mt-2 text-primary-300`}>{time}</time>
+              <time className={`block text-${!userMsg ? "right" : ""} text-xs mt-2 text-primary-300`}>{messageTime ?? "--:--"}</time>
             </div>
         </div>
     )
