6a56409ed3defe030a50ead0a9399a17d449c398
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-20 17:42:27 +0300

    UPDATE pass props to ChatList

--- src/features/sidebox/SideBox.jsx
+++ src/features/sidebox/SideBox.jsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 import { Sidebar } from "features/sidebar"
 import { ChatList } from "features/chats"
 
-function SideBox() {
+function SideBox(props) {
     const [tap, setTap] = useState("chats")
 
     return (
         <section className="basis-[30rem] shrink border-r border-primary-800 flex">
            <Sidebar tap={tap} setTap={setTap} />
            <article className="py-6 px-4 flex-1">
-            <ChatList />
+            <ChatList {...props} />
            </article> 
         </section>
     )
