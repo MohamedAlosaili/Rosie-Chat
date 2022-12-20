53e81ff044e6f37f8376ee35de6d14b9283b419a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-20 21:55:13 +0300

    UPDATE initiate local state

--- src/features/chatbox/Form.jsx
+++ src/features/chatbox/Form.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 import PropTypes from "prop-types"
+import { nanoid } from "nanoid"
 
+import { collection, addDoc, serverTimestamp } from "firebase/firestore"
+
+import { db, auth } from "rosie-firebase"
 import { send } from "imgs"
 
-function Form() {
+function Form({ selectedChat }) {
+
+    const [message, setMessage] = useState("")
+
+
     return (
         <form 
             onSubmit={(e) => e.preventDefault()}
@@ -11,6 +20,8 @@ function Form() {
             <input 
                 type="text" 
                 placeholder="Type a message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
                 className="flex-1 text-primary-200 px-4 focus:outline-none bg-transparent"
             />
             <button className="w-10 aspect-square grid place-items-center rounded-50 bg-accent hover:bg-accent-600">
