2bc9543f39e9a698f684c1c40c815608f4363e82
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-26 21:06:50 +0300

    ADD hide/show password icon

--- src/imgs/index.js
+++ src/imgs/index.js
@@ -2,6 +2,8 @@ import googleLogo from "./auth/Google_Logo.svg";
 import checked from "./auth/checkmark-checked.svg";
 import notChecked from "./auth/checkmark-not-checked.svg";
 import verifyEmail from "./auth/verify-email.svg";
+import hidePassword from "./auth/hide-password.svg";
+import showPassword from "./auth/show-password.svg";
 
 import defaultAvatar from "./sidebar/default-avatar.png";
 import chat from "./sidebar/nav-chat.svg";
@@ -18,6 +20,8 @@ export {
   checked,
   notChecked,
   verifyEmail,
+  hidePassword,
+  showPassword,
   defaultAvatar,
   chat,
   contacts,
