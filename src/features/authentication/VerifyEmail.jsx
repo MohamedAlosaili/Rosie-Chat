bde60800140c095a14e31dec38c828f866e16804
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-12-26 16:21:40 +0300

    REFACTOR extract logic into separate custom hooks

--- src/features/authentication/VerifyEmail.jsx
+++ src/features/authentication/VerifyEmail.jsx
@@ -1,106 +1,104 @@
-import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
+import { useEffect, useState } from "react"
+import PropTypes from "prop-types"
 
-import { signOut, updateProfile } from "firebase/auth";
-import { useSendEmailVerification } from "react-firebase-hooks/auth";
+import { signOut, updateProfile } from "firebase/auth"
+import { useSendEmailVerification } from "react-firebase-hooks/auth"
 
-import { auth } from "rosie-firebase";
-import { Button, StatusMessage } from "components";
-import { verifyEmail, defaultAvatar } from "imgs";
+import { auth } from "rosie-firebase"
+import { Button, StatusMessage } from "components"
+import { verifyEmail, defaultAvatar } from "imgs"
 
+function useSendVerification(user, isTimerRunning, resetTimer) {
+  const [sendEmailVerification, sending, error] = useSendEmailVerification(auth)
+  const [isEmailSend, setIsEmailSend] = useState(() => {
+    const emailVerify = JSON.parse(sessionStorage.getItem("email-verify"))
 
-const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
-  const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
-  const [timer, setTimer] = useState(() => {
-    const sessionTimer = JSON.parse(sessionStorage.getItem("timer"));
-    const verify = JSON.parse(sessionStorage.getItem("email-verify"));
+    if(emailVerify?.isSend && emailVerify?.email === user.email) return emailVerify?.isSend
+    else return false
+  })
 
-    if (
-      sessionTimer &&
-      !sessionTimer.isFinished &&
-      verify?.email === user.email
-    ) {
-      return sessionTimer;
-    }
-
-    const newTimer = { minutes: "01", seconds: "00", isFinished: false };
-    return newTimer;
-  });
-
-  // Update user info ( displayName & photoURL ) & send verification email
-  let interval;
   useEffect(() => {
-    interval = setInterval(countDown, 1000);
-
     const userInfo = JSON.parse(sessionStorage.getItem("user-info"))
-    if(userInfo && !user.displayName) {
-      const { email, name } = userInfo
 
-      if(email.toLowerCase() === user.email.toLowerCase()) {
+    if(userInfo && !user.displayName) {
+        const { name } = userInfo
+
         updateProfile(user, { displayName: name, photoURL: defaultAvatar })
           .then(() => {
-            sendVerificationEmail();
-            sessionStorage.setItem("email-verify", JSON.stringify({ isSend: true, email: user.email })
-            );
+            sendVerificationEmail()
+            sessionStorage.removeItem("user-info")
+            sessionStorage.setItem("email-verify", JSON.stringify({ isSend: true, email: user.email }))
           }).catch(error => console.log(error))
-      }
+    } else if(!isEmailSend) {
+      sendVerificationEmail()
+      sessionStorage.setItem("email-verify", JSON.stringify({ isSend: true, email: user.email }))
     }
-    
-
-    return () => clearInterval(interval);
-  }, []);
-
-  // store the remaining timer in sessionStorage if the component unmount
-  useEffect(
-    () => () => sessionStorage.setItem("timer", JSON.stringify(timer)),
-    [timer]
-  );
-
-  // change the email-verify state depending on the timer or if rsend clicked
-  useEffect(() => {
-    timer.isFinished &&
-      sessionStorage.setItem("email-verify", JSON.stringify({ isSend: false, email: user.email }));
-
-    if (+timer.seconds > 0 && !timer.isFinished)
-      sessionStorage.setItem("email-verify", JSON.stringify({ isSend: true, email: user.email }));
-  }, [timer.isFinished]);
+  }, [])
 
   async function sendVerificationEmail() {
-    if (timer.isFinished) {
-      interval = setInterval(countDown, 1000);
-      setTimer({
-        minutes: "01",
-        seconds: "00",
-        isFinished: false,
-      });
+    if(!isTimerRunning) {
+      resetTimer(90)
     }
-    await sendEmailVerification();
+    await sendEmailVerification()
   }
+
+  return [sendVerificationEmail, error]
+}
+
+function useTimer(user) {
+  const [counter, setCounter] = useState(() => {
+    const storedCounter = JSON.parse(sessionStorage.getItem("counter")) 
+    const verifyEmail = JSON.parse(sessionStorage.getItem("email-verify"))
+
+    if(storedCounter > 0 && verifyEmail?.email === user.email) return storedCounter
+    return 90
+  })
+  const [timer, setTimer] = useState(() => countDown())
+  const [isTimerRunning, setIsTimerRunning] = useState(true)
+
+  useEffect(() => {
+    let timeOut
+    if(counter > 0) {
+      !isTimerRunning && setIsTimerRunning(true)
+
+      timeOut = setTimeout(() => {
+        setCounter(prevCounter => prevCounter - 1)
+      }, 1000)
+    } else {
+      setIsTimerRunning(false)
+    }
+
+    return () => clearTimeout(timeOut)
+  })
+
+  useEffect(() => {
+    setTimer(countDown())
+
+    return () => sessionStorage.setItem("counter", counter)
+  }, [counter])
+
 
   function countDown() {
-    setTimer(({ minutes, seconds, isFinished }) => {
-      let newMin = 0;
-      let newSec = +seconds === 0 ? 59 : +seconds - 1;
+    const min = Math.floor(counter / 60)
+    const sec = counter % 60
 
-      if (+minutes === 0 && +seconds === 0) {
-        clearInterval(interval);
-        return { minutes: `00`, seconds: `00`, isFinished: true };
-      }
-
-      return {
-        minutes: `0${newMin}`,
-        seconds: newSec < 10 ? `0${newSec}` : newSec,
-        isFinished,
-      };
-    });
+    return `0${min}:${sec < 10 ? "0" : ""}${sec}`
   }
+
+  return [timer, isTimerRunning, setCounter]
+}
+
+function VerifyEmail({ user, selectedTap, setSelectedTap }) {
+
+  const [timer, isTimerRunning, resetTimer] = useTimer(user) 
+  const [sendVerificationEmail, error] = useSendVerification(user, isTimerRunning, resetTimer)
 
   function backToSignInPage() {
-    selectedTap === "signup" && setSelectedTap("signin");
+    selectedTap === "signup" && setSelectedTap("signin")
 
-    signOut(auth);
+    signOut(auth)
   }
-
+  
   return (
     <div className="bg-primary-100 dark:bg-primary-800 p-6 rounded-2xl text-center">
       {error && <StatusMessage message={error.code} type="error" />}
@@ -119,16 +117,12 @@ const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
         An email has been sent to {user.email} check your spam folder if you
         have not received the email
       </p>
-      <div className="my-4">
-        {timer.minutes}:{timer.seconds}
-      </div>
+      <div className="my-4">{timer}</div>
       <div className="flex gap-4">
         <Button
-          disabled={!timer.isFinished}
+          disabled={isTimerRunning}
           handleClick={sendVerificationEmail}
-          additionClasses={`flex-1 text-sm ${
-            !timer.isFinished ? "cursor-not-allowed opacity-30" : ""
-          }`}
+          additionClasses={`flex-1 text-sm ${isTimerRunning ? "cursor-not-allowed opacity-30" : ""}`}
         >
           Resend Email
         </Button>
@@ -137,13 +131,13 @@ const VerifyEmail = ({ user, selectedTap, setSelectedTap }) => {
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
 
 VerifyEmail.propType = {
   user: PropTypes.object.isRequired,
   selectedTap: PropTypes.string,
   setSelectedTap: PropTypes.func,
-};
+}
 
-export default VerifyEmail;
+export default VerifyEmail
