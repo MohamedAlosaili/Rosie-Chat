d8760f8d03a7d89ffde62e0af1284ec8a1818617
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-02 22:19:33 +0300

    REMOVE props to use ChatContext & UserContext instead

--- src/features/sidebox/SideBox.jsx
+++ src/features/sidebox/SideBox.jsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 import { Sidebar } from "features/sidebar"
 import { ChatList } from "features/chats"
 
-function SideBox(props) {
+function SideBox() {
     const [tap, setTap] = useState("chats")
 
     return (
         <section className="basis-[30rem] shrink border-r border-primary-800 flex">
            <Sidebar tap={tap} setTap={setTap} />
            <article className="py-6 px-4 flex-1">
-            <ChatList {...props} />
+            <ChatList />
            </article> 
         </section>
     )
