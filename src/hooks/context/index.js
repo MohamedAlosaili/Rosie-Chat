9e96d6642aa0bc841f8822675184bdcd58d21eb8
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-02 22:08:57 +0300

    ADD index file to export contexts

---
+++ src/hooks/context/index.js
@@ -0,0 +1,4 @@
+import { UserContextProvider, UserContext } from "./UserContext";
+import { ChatContextProvider, ChatContext } from "./ChatContext";
+
+export { UserContextProvider, UserContext, ChatContextProvider, ChatContext };
