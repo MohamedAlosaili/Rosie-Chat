c14d85c1f18c7adc102de32e08c46a07c497f248
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-02 22:34:47 +0300

    REFACTOR reduce props & use Intl API
    For simplicity, I reduce the props to a chat object that contains all chat properties,
    and isSelected which is determines whether it selected or not.
    Also, I consume the changeChat from ChatContext to handle clicking on chat

--- src/features/chats/chat.jsx
+++ src/features/chats/chat.jsx
@@ -1,42 +1,49 @@
-import { memo } from "react"
+import { memo, useContext } from "react"
 import PropTypes from "prop-types"
 
+import { ChatContext } from "hooks/context"
 
-const Chat = ({ 
-  photoURL, 
-  name, 
-  lastMsgTime, 
-  lastMsg, 
-  unreadMsgs, 
-  setSelectedChat, 
-  selected
-}) => {
+const Chat = ({ chat, isSelected }) => {
 
-    console.log("<Chat />")  
-    return (
+  const { changeChat } = useContext(ChatContext)  
+
+  // This block needs some refactor
+  const today = new Date()
+  const messageDate = new Date(chat.lastMsg.createdAt?.toDate())
+  const timeDiffrence = Math.round((today - messageDate) / 1000 / 60 / 60 / 24)
+  let options = {}
+  if(timeDiffrence < 1) {
+    options.hour = "numeric" 
+    options.minute = "numeric"
+  } else if(timeDiffrence < 7) {
+    options.weekday = "short"
+  }
+  const timeFormater = new Intl.DateTimeFormat("en-US", options)
+
+  return (
         <li 
-          onClick={setSelectedChat}
+          onClick={() => changeChat(chat)}
           className={`grid grid-cols-[auto_1fr] text-sm rounded-xl gap-4 p-4 mb-2 last:mb-0 cursor-pointer transition-colors overflow-hidden select-none 
                       relative z-10 before:absolute before:inset-0 before:opacity-50 before:-z-10 before:transition-colors 
-                      dark:active:before:bg-primary-700 dark:hover:before:bg-primary-800 ${selected ? "dark:bg-primary-800" : ""}`
+                      dark:active:before:bg-primary-700 dark:hover:before:bg-primary-800 ${isSelected ? "dark:bg-primary-800" : ""}`
           }
         >
           <img 
-            src={photoURL} 
+            src={chat.photoURL} 
             alt="chat image" 
             className="w-14 h-14 object-cover rounded-50"
           />
           <div className="min-w-0 flex-1">
             <div className="flex justify-between items-center gap-2 mb-2">
-              <h3 className="text-base font-medium dark:text-primary-200 truncate">{name}</h3>
-              <time className="whitespace-nowrap">{lastMsgTime}</time>
+              <h3 className="text-base font-medium dark:text-primary-200 truncate">{chat.name}</h3>
+              <time className="whitespace-nowrap">{timeFormater.format(chat.lastMsg.createdAt.toDate())}</time>
             </div>   
             <div className="flex justify-between items-center gap-2">
-              <p className="truncate">{lastMsg}</p>
+              <p className="truncate">{chat.lastMsg.message}</p>
               {
-                unreadMsgs > 0 &&
+                chat.unreadMsgs > 0 &&
                 <span className="bg-accent text-xs dark:text-primary-200 rounded-full py-[0.3rem] px-2 font-semibold leading-none">
-                    {unreadMsgs}
+                    {chat.unreadMsgs}
                 </span>
               }
             </div>
@@ -51,13 +58,13 @@ Chat.proptypes = {
     lastMsgTime: PropTypes.string, 
     lastMsg: PropTypes.string, 
     unreadMsg: PropTypes.number,
-    selected: PropTypes.bool
+    isSelected: PropTypes.bool
 }
 
 Chat.defaultProps = {
     chatImg: "", // placeholder image
     unreadMsg: 0,
-    selected: false
+    isSelected: false
 }
 
 export default memo(Chat)
