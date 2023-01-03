7a690293c622a07e5936746a871150c5d4ab7972
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-03 13:16:04 +0300

    ADD message & group & direct templates

--- src/util/objectsTemplate.js
+++ src/util/objectsTemplate.js
@@ -1,13 +1,15 @@
 // Get userDoc by auth.currentUser.uid
 const userDoc = {
-  displayName: "string",
-  email: "string",
-  photoURL: "string",
-  chats: "Array",
+  displayName: null, // string
+  email: null, // string
+  photoURL: null, // string
+  isOnline: false, // boolean
+  chats: [], // array
+  friends: [], // array
 };
 
 // Individual chat object of ChatList
-export const selectedChatObj = {
+const selectedChatObj = {
   id: null, // "string"
   isGroup: false, // "boolean"
   lastMsg: {
@@ -19,6 +21,35 @@ export const selectedChatObj = {
   unreadMsgs: 0, // "number"
 };
 
-const groupDoc = {};
+const messageDoc = {
+  id: null, // string
+  uid: null, // string
+  displayName: null, // string
+  photoURL: null, // string
+  message: null, // string
+  createdAt: null, // timestamp
+};
 
-const directDoc = {};
+const groupDoc = {
+  id: null, // string
+  isGroup: true, // boolean
+  name: null, // string
+  photoURL: null, // string
+  lastMsg: {
+    message: null, // string
+    createdAt: null, // timestamp
+  },
+};
+
+const directDoc = {
+  id: null, // string
+  isGroup: false, // boolean
+  name: null, // string
+  photoURL: null, // string
+  lastMsg: {
+    message: null, // string
+    createdAt: null, // timestamp
+  },
+};
+
+export { userDoc, selectedChatObj, messageDoc, groupDoc, directDoc };
