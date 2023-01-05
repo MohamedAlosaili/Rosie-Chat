2099da3ea9a6d6bc46715b776afb2eefffa4d67a
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-05 15:08:33 +0300

    FIX clear file input value & use Modal component
    If the user chooses a file then close or reopen the review and choose the same file
    changeFile handler won't work because the value doesn't change so I clear the
    value every time a user closes the review or sends the file to prevent this bug

--- src/features/chatbox/FileInput.jsx
+++ src/features/chatbox/FileInput.jsx
@@ -1,74 +1,83 @@
 import { useRef, useState } from "react";
 import PropTypes from "prop-types";
 
-import { fileIcon } from "imgs";
-import { Button, Input } from "components";
 import { useSendMessage } from "hooks";
+import { fileIcon } from "imgs";
+import { Button, Input, Modal } from "components";
 
 const FileInput = (props) => {
+  const inputFileRef = useRef(null);
   const [preview, setPreview] = useState(null);
-  const [message, setMessage, sendMessage, loading, fileRef] = useSendMessage(
+  const [message, setMessage, sendMessage, loading, file] = useSendMessage(
     "caption",
     { ...props },
     setPreview
   );
 
+  // Note: if the user chooses the same file this handler won't run because the value isn't change
   function changeFile(e) {
+    console.log("entered but not changed");
     if (e.target.files.length > 0) {
-      const file = e.target.files[0];
-      fileRef.current = file;
+      const fileObj = e.target.files[0];
+      file.current = fileObj;
       // Generate URL for file preview before sending it
-      setPreview(URL.createObjectURL(file));
+      setPreview(URL.createObjectURL(fileObj));
     }
   }
 
+  //
   function closePreview() {
-    fileRef.current = null;
+    inputFileRef.current.value = "";
+    file.current = null;
     setPreview(null);
     setMessage({ caption: "" });
   }
-
+  console.log(preview);
   return (
     <>
-      {preview && (
-        <div className="fixed inset-0 z-10 grid place-items-center p-4 before:absolute before:inset-0 before:opacity-30 before:-z-10 before:bg-black">
-          <div className="p-6 rounded-xl dark:bg-primary-800">
-            <img src={preview} className="w-80 rounded-xl mb-2" />
-            <Input
-              type="text"
-              placeholder="Caption"
-              name="caption"
-              value={message.caption}
-              setValue={setMessage}
-            />
-            <div className="flex justify-between mt-4">
-              <Button
-                disabled={loading}
-                type="button"
-                additionClasses="w-32"
-                handleClick={closePreview}
-              >
-                Cancel
-              </Button>
-              <Button
-                disabled={loading}
-                type="button"
-                additionClasses="w-32"
-                handleClick={sendMessage}
-              >
-                {loading ? "Sending..." : "Send"}
-              </Button>
-            </div>
+      <Modal isHidden={preview === null} closeModal={closePreview}>
+        <div className="p-6 rounded-xl dark:bg-primary-800">
+          <img src={preview} className="w-80 rounded-xl mb-2" />
+          <Input
+            type="text"
+            placeholder="Caption"
+            name="caption"
+            value={message.caption}
+            setValue={setMessage}
+          />
+          <div className="flex justify-between mt-4">
+            <Button
+              disabled={loading}
+              type="button"
+              additionClasses="w-32"
+              handleClick={closePreview}
+            >
+              Cancel
+            </Button>
+            <Button
+              disabled={loading}
+              type="button"
+              additionClasses="w-32"
+              handleClick={sendMessage}
+            >
+              {loading ? "Sending..." : "Send"}
+            </Button>
           </div>
         </div>
-      )}
+      </Modal>
       <label htmlFor="file" className="cursor-pointer px-2">
         <img
           src={fileIcon}
           className="transition w-6 dark:invert-[0.7] dark:hover:invert"
         />
       </label>
-      <input type="file" id="file" className="hidden" onChange={changeFile} />
+      <input
+        ref={inputFileRef}
+        type="file"
+        id="file"
+        className="hidden"
+        onChange={changeFile}
+      />
     </>
   );
 };
