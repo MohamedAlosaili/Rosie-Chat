1ef8a5eab2e3c51a92a96d170eb5fc407e189c9e
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-08 18:46:40 +0300

    REFACTOR lift StatusMessage component up

--- src/features/authentication/VerifyEmail/index.jsx
+++ src/features/authentication/VerifyEmail/index.jsx
@@ -99,7 +99,7 @@ function useTimer(user) {
 
 function VerifyEmail({ user, selectedTap, setSelectedTap }) {
   const [timer, isTimerRunning, resetTimer] = useTimer(user);
-  const [sendVerificationEmail, error] = useSendVerification(
+  const [sendVerificationEmail, sendingError] = useSendVerification(
     user,
     isTimerRunning,
     resetTimer
@@ -112,39 +112,44 @@ function VerifyEmail({ user, selectedTap, setSelectedTap }) {
   }
 
   return (
-    <div className="bg-primary-100 dark:bg-primary-800 p-6 rounded-2xl text-center">
-      {error && <StatusMessage message={error.code} type="error" />}
-      <h1 className="text-2xl font-bold mb-6 text-primary-900 dark:text-primary-200">
-        Verify your email
-      </h1>
-      <h3 className="text-[0.9rem] text-primary-900 dark:text-primary-200">
-        You will need to verify your email to compelte registeration
-      </h3>
-      <img
-        src={verifyEmail}
-        alt="Verify Email Image"
-        className="w-40 mx-auto my-6"
-      />
-      <p className="text-[0.9rem] ">
-        An email has been sent to {user.email} check your spam folder if you
-        have not received the email
-      </p>
-      <div className="my-4">{timer}</div>
-      <div className="flex gap-4">
-        <Button
-          disabled={isTimerRunning}
-          handleClick={sendVerificationEmail}
-          additionClasses={`flex-1 text-sm ${
-            isTimerRunning ? "cursor-not-allowed opacity-30" : ""
-          }`}
-        >
-          Resend Email
-        </Button>
-        <Button handleClick={backToSignInPage} additionClasses="flex-1 text-sm">
-          Sign in
-        </Button>
+    <>
+      {sendingError && <StatusMessage message={error?.code} type="error" />}
+      <div className="bg-primary-100 dark:bg-primary-800 p-6 rounded-2xl text-center">
+        <h1 className="text-2xl font-bold mb-6 text-primary-900 dark:text-primary-200">
+          Verify your email
+        </h1>
+        <h3 className="text-[0.9rem] text-primary-900 dark:text-primary-200">
+          You will need to verify your email to compelte registeration
+        </h3>
+        <img
+          src={verifyEmail}
+          alt="Verify Email Image"
+          className="w-40 mx-auto my-6"
+        />
+        <p className="text-[0.9rem] ">
+          An email has been sent to {user.email} check your spam folder if you
+          have not received the email
+        </p>
+        <div className="my-4">{timer}</div>
+        <div className="flex gap-4">
+          <Button
+            disabled={isTimerRunning}
+            handleClick={sendVerificationEmail}
+            additionClasses={`flex-1 text-sm ${
+              isTimerRunning ? "cursor-not-allowed opacity-30" : ""
+            }`}
+          >
+            Resend Email
+          </Button>
+          <Button
+            handleClick={backToSignInPage}
+            additionClasses="flex-1 text-sm"
+          >
+            Sign in
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
