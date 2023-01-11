1a179412e4783524bb2c05d5618c619487cfdbd1
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-12 00:01:45 +0300

    ADD useSendVerification hook
    useSendVerification was internally in VerifyEmail
    I move it here to keep all hooks under the hooks directory

---
+++ src/hooks/useSendVerification.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useRef } from "react";
+
+import { updateProfile } from "firebase/auth";
+import { useSendEmailVerification } from "react-firebase-hooks/auth";
+
+import { useStorage } from "hooks";
+import { auth } from "rosie-firebase";
+import { defaultAvatar } from "imgs";
+
+function useSendVerification(user, isTimerRunning, startTimer) {
+  const [sendEmailVerification, , error] = useSendEmailVerification(auth);
+  // TODO: need improvement
+  const [getEmailVerify, setEmailVerify] = useStorage(
+    "sessionStorage",
+    "email-verify"
+  );
+  const [getUserInfo, , removeUserInfo] = useStorage(
+    "sessionStorage",
+    "user-info"
+  );
+  const { current: isEmailSend } = useRef(() => {
+    if (getEmailVerify?.isSend && getEmailVerify?.email === user.email)
+      return getEmailVerify.isSend;
+    else return false;
+  });
+
+  useEffect(() => {
+    if (getUserInfo && !user.displayName) {
+      updateProfile(user, {
+        displayName: getUserInfo.name,
+        photoURL: defaultAvatar,
+      })
+        .then(() => {
+          sendVerificationEmail();
+          removeUserInfo();
+          setEmailVerify({ isSend: true, email: user.email });
+        })
+        .catch((error) => console.log(error));
+    } else if (!isEmailSend()) {
+      sendVerificationEmail();
+      setEmailVerify({ isSend: true, email: user.email });
+    }
+  }, []);
+
+  async function sendVerificationEmail() {
+    if (!isTimerRunning) {
+      startTimer();
+    }
+    await sendEmailVerification();
+  }
+
+  return [sendVerificationEmail, error];
+}
+
+export default useSendVerification;
