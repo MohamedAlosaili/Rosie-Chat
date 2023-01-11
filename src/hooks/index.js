2d88747427abded317d5c6abe1ff39822c041616
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-12 00:03:46 +0300

    ADD useStorage & useTimer & useSendVerification

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,17 @@
+import useStorage from "./useStorage";
+
+import useTimer from "./useTimer";
+import useSendVerification from "./useSendVerification";
+
 import useSendMessage from "./useSendMessage";
 import useFile from "./useFile";
 import useError from "./useError";
 
-export { useSendMessage, useFile, useError };
+export {
+  useStorage,
+  useTimer,
+  useSendVerification,
+  useSendMessage,
+  useFile,
+  useError,
+};
