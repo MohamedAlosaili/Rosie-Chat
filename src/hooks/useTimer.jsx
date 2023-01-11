02624f606accf2c7fc8e3230f8f012794679a19f
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-12 00:01:00 +0300

    ADD useTimer hook
    useTimer was internally in VerifyEmail
    I move it here to keep all hooks under the hooks directory

---
+++ src/hooks/useTimer.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+import { useStorage } from "hooks";
+import { useRef } from "react";
+
+function useTimer(initialValue = 90) {
+  const [getStorageCounter, setStorageCounter] = useStorage(
+    "sessionStorage",
+    "counter"
+  );
+  const [counter, setCounter] = useState(
+    () => +getStorageCounter || initialValue
+  );
+  const [timer, setTimer] = useState(() => countDown());
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const interval = useRef(null);
+
+  useEffect(() => {
+    startTimer();
+    return () => stopTimer();
+  }, []);
+
+  useEffect(() => {
+    if (counter <= 0) stopTimer();
+
+    setTimer(countDown());
+    setStorageCounter(counter);
+  }, [counter]);
+
+  function startTimer() {
+    counter === 0 && setCounter(initialValue);
+    setIsTimerRunning(true);
+
+    interval.current = setInterval(() => {
+      setCounter((prevCounter) => prevCounter - 1);
+    }, 1000);
+  }
+
+  function stopTimer() {
+    setIsTimerRunning(false);
+    clearInterval(interval.current);
+    interval.current = null;
+  }
+
+  function countDown() {
+    const min = Math.floor(counter / 60);
+    const sec = counter % 60;
+
+    return `0${min}:${sec < 10 ? "0" : ""}${sec}`;
+  }
+
+  return [timer, isTimerRunning, startTimer];
+}
+
+export default useTimer;
