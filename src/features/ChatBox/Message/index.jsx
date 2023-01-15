8f945b94fb1d05ca87da05d147cf9a885e9104da
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-15 16:19:31 +0300

    UPDATE use Video & Image components

--- src/features/ChatBox/Message/index.jsx
+++ src/features/ChatBox/Message/index.jsx
@@ -2,6 +2,7 @@ import { memo } from "react";
 
 import PropTypes from "prop-types";
 
+import { Image, Video } from "components";
 import { auth } from "rosie-firebase";
 import uniqolor from "uniqolor";
 
@@ -37,9 +38,8 @@ const Message = ({ messageObject, prevMsgSender, selectedChat }) => {
 
   return (
     <div
-      className={`flex justify-start gap-2 ${
-        currentUserMsg ? "flex-row-reverse" : ""
-      } my-2`}
+      className={`flex justify-start gap-2 ${currentUserMsg ? "flex-row-reverse" : ""
+        } my-2`}
     >
       {otherMebmerMsgs && (
         <div className="w-8 h-8">
@@ -47,22 +47,15 @@ const Message = ({ messageObject, prevMsgSender, selectedChat }) => {
             <img
               src={photoURL}
               alt={`${displayName} avatar`}
-              className={`w-full object-cover ${
-                photoURL.includes("default-avatar") ? "bg-[var(--color)]" : ""
-              } rounded-50 p-px`}
+              className={`w-full object-cover ${photoURL.includes("default-avatar") ? "bg-[var(--color)]" : ""
+                } rounded-50 p-px`}
               style={userColor}
             />
           )}
         </div>
       )}
       <div
-        className={`max-w-[70%] p-2 rounded-xl 
-                        ${
-                          currentUserMsg
-                            ? "dark:bg-accent"
-                            : "dark:bg-primary-800"
-                        } dark:text-primary-200
-                `}
+        className={`max-w-[70%] p-2 rounded-xl ${currentUserMsg ? "dark:bg-accent" : "dark:bg-primary-800"} dark:text-primary-200`}
       >
         {otherMebmerMsgs && !isTheSameSender && (
           <h3
@@ -75,24 +68,19 @@ const Message = ({ messageObject, prevMsgSender, selectedChat }) => {
         <div className="flex flex-col gap-2">
           {message.file?.url && (
             <div
-              className={`rounded-xl overflow-hidden bg-primary-100 ${
-                message.text === "" ? "mb-1" : ""
-              }`}
+              className={`rounded-xl overflow-hidden bg-primary-100 ${message.text === "" ? "mb-1" : ""
+                }`}
             >
-              <img
-                src={message.file.url}
-                alt={`Image from ${displayName}`}
-                className="max-w-full md:min-w-[15rem] block"
-              />
+              {message.file?.type?.startsWith("image") ? (
+                <Image img={message.file} />
+              ) : (
+                <Video video={message.file} />
+              )}
             </div>
           )}
           {message.text && <p>{message.text}</p>}
         </div>
-        <time
-          className={`block mt-1 leading-none ${
-            !currentUserMsg ? "text-right" : ""
-          } text-[0.65rem] opacity-60`}
-        >
+        <time className={`block mt-1 leading-none ${!currentUserMsg ? "text-right" : ""} text-[0.65rem] opacity-60`}>
           {dateFormater.format(createdAt?.toDate()) ?? "--:--"}
         </time>
       </div>
