e3b8ffdb6902b51ebb60afeee5d1c952b29ab11c
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-15 16:07:00 +0300

    ADD Video & Image components

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import StatusMessage from "./StatusMessage";
 import Input from "./Input";
 import Button from "./Button";
 import Modal from "./Modal";
+import Image from "./Image";
+import Video from "./Video";
 
-export { StatusMessage, Input, Button, Modal };
+export { StatusMessage, Input, Button, Modal, Image, Video };
