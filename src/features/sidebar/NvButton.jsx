c69d7d75a9d7071eaf2c169140d2c8b762de36cf
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-17 16:25:34 +0300

    UPDATE add motion button & change transition depend on the button

--- src/features/sidebar/NvButton.jsx
+++ src/features/sidebar/NvButton.jsx
@@ -1,17 +1,18 @@
 import { memo } from "react";
 import PropTypes from "prop-types";
 
+import { motion } from "framer-motion";
+
 import { defaultAvatar } from "imgs";
+import { buttonMotion } from "util";
 
 const NvButton = ({ handleClick, tap, img, btnTap, userName }) => {
   const buttonStyle =
     btnTap !== "profile"
-      ? `p-4 rounded-xl ${
-          tap === btnTap ? "bg-accent active" : "dark:hover:bg-primary-700"
-        }`
-      : `p-1 rounded-50 ring-4 ${
-          tap === btnTap ? "ring-accent" : "ring-transparent"
-        }`;
+      ? `transition-colors p-4 rounded-xl ${tap === btnTap ? "bg-accent active" : "dark:hover:bg-primary-700"
+      }`
+      : `transition-shadow p-1 rounded-50 ring-4 ${tap === btnTap ? "ring-accent" : "ring-transparent"
+      }`;
 
   const imgStyle =
     btnTap === "profile"
@@ -20,9 +21,11 @@ const NvButton = ({ handleClick, tap, img, btnTap, userName }) => {
 
   return (
     <div className="relative">
-      <button
+      <motion.button
+        whileHover={buttonMotion.hover}
+        whileTap={buttonMotion.tap}
         onClick={handleClick}
-        className={`transition-colors transition-shadow group peer active:scale-[0.97] ${buttonStyle}`}
+        className={`group peer ${buttonStyle}`}
       >
         <img
           src={img ?? (btnTap === "profile" && defaultAvatar)}
@@ -30,10 +33,10 @@ const NvButton = ({ handleClick, tap, img, btnTap, userName }) => {
           className={`${imgStyle}`}
           onError={(e) => (e.target.src = defaultAvatar)}
         />
-      </button>
+      </motion.button>
       <span
-        className={`absolute left-[150%] top-1/2 -translate-y-1/2 block bg-primary-200 text-primary-700 font-medium py-1 px-4 rounded-lg 
-                            invisible peer-hover:visible opacity-0 peer-hover:opacity-100 translate-x-[15%] peer-hover:translate-x-0 transition z-10`}
+        className={`transition-all absolute left-[150%] top-1/2 -translate-y-1/2 block bg-primary-200 text-primary-700 font-medium py-1 px-4 rounded-lg 
+                            invisible peer-hover:visible opacity-0 peer-hover:opacity-100 translate-x-[15%] peer-hover:translate-x-0 z-10`}
       >
         {btnTap[0].toUpperCase() + btnTap.slice(1)}
       </span>
