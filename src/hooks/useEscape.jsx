3bf6881c08197ea60933b3127d26a471978e94d3
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-17 16:04:33 +0300

    ADD useEscape that handle clicking on ESC button

---
+++ src/hooks/useEscape.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+function useEscape(closeHandler) {
+
+    useEffect(() => {
+        window.addEventListener("keydown", close);
+        return () => window.removeEventListener("keydown", close);
+    }, []);
+
+
+    function close(e) {
+        if (e.key === "Escape") {
+            closeHandler();
+        }
+    }
+}
+
+export default useEscape
