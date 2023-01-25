d510f13bc9abe071edec8784664584f7fa15f12e
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-25 17:42:36 +0300

    REFACTOR query a group of document depend on chats array of userDoc
    To keep data stored in the firebase simple I reduced the chats array
    content to be just the ids of chats and to query them in ChatsList
    component using the where method where the doc id must be in chats array

--- src/features/ChatsList/index.jsx
+++ src/features/ChatsList/index.jsx
@@ -1,7 +1,14 @@
 import { useContext } from "react";
 
-import { UserContext, ChatContext } from "hooks/context";
+import { useCollectionData } from "react-firebase-hooks/firestore"
+import { collection, orderBy, query, where } from "firebase/firestore";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { BsExclamationCircleFill } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+
+import { db } from "rosie-firebase"
 import Chat from "./Chat";
+import { UserContext, ChatContext } from "hooks/context";
 import { search } from "imgs";
 
 function ChatsList() {
@@ -9,9 +16,14 @@ function ChatsList() {
   const userDoc = useContext(UserContext);
   const { selectedChat } = useContext(ChatContext);
 
-  console.log("### userDoc bug ###", userDoc);
+  const qu = query(
+    collection(db, "chats"),
+    where("id", "in", userDoc.chats),
+    orderBy("lastMsg.createdAt")
+  )
+  const [chats, userChatsLoading, userChatsError] = useCollectionData(qu)
 
-  const userChats = userDoc?.chats.map((chat) => (
+  const userChats = chats?.map((chat) => (
     <Chat key={chat.id} chat={chat} isSelected={chat.id === selectedChat.id} />
   ));
 
@@ -25,13 +37,27 @@ function ChatsList() {
           className={`peer w-full focus:outline-none rounded-full px-4 pr-10 py-2 text-sm dark:bg-primary-800 dark:text-primary-200 transition-colors focus:placeholder:transition-colors
                       dark:focus:placeholder:text-primary-200 border-2 dark:border-primary-700 dark:focus:border-accent `}
         />
-        <img
-          src={search}
-          alt="Search icon"
-          className="absolute right-4 top-1/2 -translate-y-1/2 invert-[0.7] peer-focus:invert-[0.9] w-4"
+        <BiSearchAlt
+          size={20}
+          className={`transition-colors absolute right-4 top-1/2 -translate-y-1/2 
+          dark:peer-focus:text-primary-200`
+          }
         />
       </form>
-      <ul>{userChats}</ul>
+      {/* TODO: Status need improvements */}
+      {userChatsLoading && (
+        <div className="flex items-center gap-2 justify-center mt-4 dark:text-primary-200">
+          <BsExclamationCircleFill size={20} className="animate-spin" />
+          Loading chats...
+        </div>
+      )}
+      {userChatsError && (
+        <div className="flex items-center gap-2 justify-center mt-4 dark:text-primary-200">
+          <AiOutlineLoading3Quarters size={20} />
+          {userChatsError.code}
+        </div>
+      )}
+      {userChats && <ul>{userChats}</ul>}
     </section>
   );
 }
