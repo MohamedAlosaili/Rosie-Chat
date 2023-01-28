b08152e41df8bf8453178e23c3149b38ce79ab67
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-28 23:35:13 +0300

    ADD Backdrop to component global export

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import StatusMessage from "./StatusMessage";
 import Input from "./Input";
 import Button from "./Button";
 import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 import Image from "./Image";
 import Video from "./Video";
 
-export { StatusMessage, Input, Button, Modal, Image, Video };
+export { StatusMessage, Input, Button, Modal, Backdrop, Image, Video };
