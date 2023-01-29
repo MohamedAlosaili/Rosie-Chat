65ede6d924e502a210f18fba52abe8ade1a9989f
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-29 18:09:28 +0300

    ADD SearchForm component

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,15 @@ import Modal from "./Modal";
 import Backdrop from "./Backdrop";
 import Image from "./Image";
 import Video from "./Video";
+import SearchForm from "./SearchForm";
 
-export { StatusMessage, Input, Button, Modal, Backdrop, Image, Video };
+export {
+  StatusMessage,
+  Input,
+  Button,
+  Modal,
+  Backdrop,
+  Image,
+  Video,
+  SearchForm,
+};
