341f3db981284c5e30a134a334964212390af164
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-01-29 18:14:51 +0300

    REFACTOR component to use SearchForm and display search results

--- src/features/ChatsList/index.jsx
+++ src/features/ChatsList/index.jsx
@@ -4,12 +4,13 @@ import { useCollectionData } from "react-firebase-hooks/firestore"
 import { collection, orderBy, query, where } from "firebase/firestore";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { BsExclamationCircleFill } from "react-icons/bs";
-import { BiSearchAlt } from "react-icons/bi";
 
-import { db } from "rosie-firebase"
+
 import Chat from "./Chat";
+import { db } from "rosie-firebase"
 import { UserContext, ChatContext } from "hooks/context";
-import { search } from "imgs";
+import { SearchForm } from "components";
+import { useSearch } from "hooks";
 
 function ChatsList() {
   // TODO: userDoc sometimes return null
@@ -22,43 +23,43 @@ function ChatsList() {
     orderBy("lastMsg.createdAt")
   )
   const [chats, userChatsLoading, userChatsError] = useCollectionData(qu)
+  const [searchValue, setSearchValue, searchResults] = useSearch("chats", chats, "name")
 
-  const userChats = chats?.map((chat) => (
+  const chatsElements = (searchValue ? searchResults : chats)?.map((chat) => (
     <Chat key={chat.id} chat={chat} isSelected={chat.id === selectedChat.id} />
   ));
 
   return (
-    <section className="flex flex-col gap-4">
-      <h1 className="dark:text-primary-200 text-2xl font-semibold">Chats</h1>
-      <form className="relative">
-        <input
-          type="search"
-          placeholder="Search..."
-          className={`peer w-full focus:outline-none rounded-full px-4 pr-10 py-2 text-sm dark:bg-primary-800 dark:text-primary-200 transition-colors focus:placeholder:transition-colors
-                      dark:focus:placeholder:text-primary-200 border-2 dark:border-primary-700 dark:focus:border-accent `}
-        />
-        <BiSearchAlt
-          size={20}
-          className={`transition-colors absolute right-4 top-1/2 -translate-y-1/2 
-          dark:peer-focus:text-primary-200`
-          }
-        />
-      </form>
-      {/* TODO: Status need improvements */}
+    <div className="flex flex-col gap-4">
+      <SearchForm
+        value={searchValue}
+        setValue={setSearchValue}
+        disabled={userChatsLoading}
+      />
       {userChatsLoading && (
         <div className="flex items-center gap-2 justify-center mt-4 dark:text-primary-200">
-          <BsExclamationCircleFill size={20} className="animate-spin" />
+          <AiOutlineLoading3Quarters size={20} className="animate-spin" />
           Loading chats...
         </div>
       )}
       {userChatsError && (
         <div className="flex items-center gap-2 justify-center mt-4 dark:text-primary-200">
-          <AiOutlineLoading3Quarters size={20} />
-          {userChatsError.code}
+          <BsExclamationCircleFill size={20} className="text-red-800" />
+          {userChatsError?.code ?? <span className="text-sm">Something went wrong while loading chats <br /> Please try again in a minute</span>}
+
         </div>
       )}
-      {userChats && <ul>{userChats}</ul>}
-    </section>
+      {(!userChatsLoading && !userChatsError && (
+        chatsElements?.length > 0
+          ? <ul>{chatsElements}</ul>
+          : (
+            <div className="text-center dark:text-primary-200 break-all">
+              <p>Results for: <span className="italic dark:text-primary-400">{searchValue}</span></p>
+              <p>No chats found.</p>
+            </div>
+          )
+      ))}
+    </div>
   );
 }
 
