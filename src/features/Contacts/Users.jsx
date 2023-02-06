48cda480eaf91fea4ccaac2efade7d19a4182c28
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-06 15:11:53 +0300

    UPDATE handle loading (using skeletonLoader) & error state

--- src/features/Contacts/Users.jsx
+++ src/features/Contacts/Users.jsx
@@ -4,12 +4,12 @@ import PropTypes from "prop-types"
 import { BsFillPersonPlusFill } from "react-icons/bs"
 
 import UserCard from "./UserCard"
-import { SearchForm, Button } from "components"
+import { SkeletonLoader, SearchForm, Button } from "components"
 import { useSearch } from "hooks"
 
-function Users({ currentUser, updateDocument, allUsers, contactTap, setContactTap }) {
+function Users({ currentUser, updateDocument, results, contactTap, setContactTap }) {
 
-    const users = contactTap === "friends" ? allUsers?.filter(user => currentUser.friends.includes(user.uid)) : allUsers
+    const users = contactTap === "friends" ? results.allUsers?.filter(user => currentUser.friends.includes(user.uid)) : results.allUsers
     const [searchValue, setSearchValue, searchResults] = useSearch("users", users, "displayName")
 
     const usersElements = (searchValue ? searchResults : users)?.map(user => (
@@ -21,17 +21,28 @@ function Users({ currentUser, updateDocument, allUsers, contactTap, setContactTa
             updateDocument={updateDocument}
         />
     ))
-
+    console.log(contactTap)
     return (
         <div className="flex flex-col gap-4">
             <SearchForm
                 value={searchValue}
                 setValue={setSearchValue}
-                disabled={usersElements?.length === 0 && searchValue === ""}
+                disabled={
+                    (usersElements?.length === 0 && searchValue === "")
+                    || results.usersLoading
+                    || results.usersError
+                }
             />
-            {usersElements?.length > 0
+            {results.usersError && (
+                <div className="flex items-center gap-2 justify-center mt-4 dark:text-primary-200">
+                    <BsExclamationCircleFill size={20} className="text-red-800" />
+                    {usersError?.code ?? <span className="text-sm">Something went wrong while loading users <br /> Please try again in a minute</span>}
+                </div>
+            )}
+            {results.usersLoading && <SkeletonLoader.Cards isUser={true} isFriend={contactTap === "friends"} />}
+            {(usersElements?.length > 0)
                 ? <ul>{usersElements}</ul>
-                : (
+                : !results.usersLoading && (
                     (searchValue === "" && contactTap === "friends")
                         ? (
                             <Button handleClick={() => setContactTap("allUsers")}>
@@ -54,7 +65,11 @@ function Users({ currentUser, updateDocument, allUsers, contactTap, setContactTa
 Users.propTypes = {
     currentUser: PropTypes.object.isRequired,
     updateDocument: PropTypes.func,
-    allUsers: PropTypes.array.isRequired,
+    results: PropTypes.shape({
+        allUsers: PropTypes.array,
+        usersLoading: PropTypes.bool,
+        usersError: PropTypes.oneOf([PropTypes.symbol, PropTypes.object])
+    }).isRequired,
     contactTap: PropTypes.string.isRequired,
     setContactTap: PropTypes.func
 }
