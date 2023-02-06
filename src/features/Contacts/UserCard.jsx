48fe6ec5f6c92a96868e02fccc40725c51e258fd
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-06 15:20:16 +0300

    UPDATE reafactor startChatting & ADD loading/error states
    I changed the way that End-To-End chats work.
    Rather than storing both ends info in a chat object I removed
    that and used the members' array to request chat(user)
    info to be sure that info is the last version of user info

--- src/features/Contacts/UserCard.jsx
+++ src/features/Contacts/UserCard.jsx
@@ -1,19 +1,20 @@
-import { memo, useContext } from "react"
+import { memo, useContext, useState } from "react"
 import PropTypes from "prop-types"
 
-import { collection, doc, getDoc, addDoc, arrayUnion, arrayRemove, serverTimestamp, updateDoc, setDoc } from "firebase/firestore"
+import { doc, getDoc, setDoc, arrayUnion, arrayRemove, serverTimestamp, updateDoc } from "firebase/firestore"
 import { BsFillPersonPlusFill, BsFillChatFill } from "react-icons/bs"
 import { FaUserMinus } from "react-icons/fa"
-import { nanoid } from "nanoid"
 
 import { db } from "rosie-firebase"
-import { Button } from "components"
+import { Button, StatusMessage } from "components"
 import { ChatContext } from "hooks/context"
 import { chatDocTemplate } from "util/objectsTemplate"
 
 const UserCard = ({ user, isFriend, currentUser, updateDocument }) => {
 
-    const { changeChat } = useContext(ChatContext)
+    const { selectedChat, changeChat } = useContext(ChatContext)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(undefined)
 
     function addNewFriend() {
         updateDocument({
@@ -29,49 +30,57 @@ const UserCard = ({ user, isFriend, currentUser, updateDocument }) => {
     }
 
     async function startChatting() {
+        setLoading(true)
+        setError(undefined)
         const currentUserId = currentUser.uid
         const otherUserId = user.uid
         const chatId = [currentUserId, otherUserId].sort().join("")
-        console.log(chatId)
-        const chatRef = doc(db, "chats", chatId)
-        const chat = await getDoc(chatRef)
 
-        if (chat.exists()) {
-            changeChat(chat.data())
-        } else {
-            // TODO: endOne & endTwo have to change into clear names
-            await setDoc(chatRef, chatDocTemplate({
-                id: chatId,
-                endOne: {
-                    uid: currentUserId,
-                    name: currentUser.displayName,
-                    photoURL: currentUser.photoURL,
-                },
-                endTwo: {
-                    uid: otherUserId,
-                    name: user.displayName,
-                    photoURL: user.photoURL,
-                },
-                lastMsg: {
-                    uid: null,
-                    message: `Say hi to `,
-                    createdAt: serverTimestamp(),
-                },
-                members: [currentUserId, otherUserId],
-            }))
-
-            await updateDocument({ chats: arrayUnion(chatId) })
-            await updateDoc(doc(db, "users", otherUserId), { chats: arrayUnion(chatId) })
-
-            const chat = await getDoc(chatRef)
-            changeChat(chat.data())
+        if (selectedChat.id === chatId) {
+            setLoading(false)
+            return
         }
+
+        try {
+
+            const chatRef = doc(db, "chats", chatId)
+            const chat = await getDoc(chatRef)
+
+            if (chat.exists()) {
+                changeChat(chat.data())
+            } else {
+                await setDoc(chatRef, chatDocTemplate({
+                    id: chatId,
+                    lastMsg: {
+                        uid: null,
+                        message: `Say hi to `,
+                        createdAt: serverTimestamp(),
+                    },
+                    members: [currentUserId, otherUserId],
+                }))
+
+                const chat = await getDoc(chatRef)
+                changeChat(chat.data())
+            }
+        } catch (error) {
+            console.log(error)
+            setError(error)
+        }
+        setLoading(false)
     }
 
     return (
         <div
             className="transition-colors grid grid-cols-[auto_1fr_auto] items-center gap-4 p-4 rounded-xl dark:hover:bg-primary-800"
         >
+            {loading && (
+                <StatusMessage type="loading" message="Opening chat..." />
+            )
+            }
+            {error && (
+                <StatusMessage type="error" message={error} />
+            )
+            }
             <img src={user.photoURL} alt={`${user.displayName} photo`} className="w-14 rounded-full" />
             <div className="w-full overflow-hidden">
                 <h3 className="dark:text-primary-200 font-semibold truncate">{user.displayName}</h3>
@@ -80,15 +89,27 @@ const UserCard = ({ user, isFriend, currentUser, updateDocument }) => {
             {isFriend
                 ? (
                     <div className="flex gap-2">
-                        <Button title="Start chatting" handleClick={startChatting}>
+                        <Button
+                            title="Start chatting"
+                            handleClick={startChatting}
+                            disabled={loading}
+                        >
                             <BsFillChatFill size={18} />
                         </Button>
-                        <Button title="Remove from friends" handleClick={removeFriend} bg="red">
+                        <Button
+                            title="Remove from friends"
+                            handleClick={removeFriend} bg="red"
+                            disabled={loading}
+                        >
                             <FaUserMinus size={18} />
                         </Button>
                     </div>
                 ) : (
-                    <Button title="Add to friends" handleClick={addNewFriend}>
+                    <Button
+                        title="Add to friends"
+                        handleClick={addNewFriend}
+                        disabled={loading}
+                    >
                         <BsFillPersonPlusFill size={18} />
                     </Button>
                 )
