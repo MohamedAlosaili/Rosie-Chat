f0649a8ca952a77c1bd9b90cf64372a08bb0a261
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-08 22:11:32 +0300

    UPDATE reorganize imgs directory & DELETE unused imgs

--- src/imgs/index.js
+++ src/imgs/index.js
@@ -1,35 +1,6 @@
-import googleLogo from "./auth/Google_Logo.svg";
-import checked from "./auth/checkmark-checked.svg";
-import notChecked from "./auth/checkmark-not-checked.svg";
-import verifyEmail from "./auth/verify-email.svg";
-import hidePassword from "./auth/hide-password.svg";
-import showPassword from "./auth/show-password.svg";
+import googleLogo from "./Google_Logo.svg";
+import verifyEmail from "./verify-email.svg";
+import defaultAvatar from "./default-avatar.png";
+import selectChat from "./select-chat.svg";
 
-import defaultAvatar from "./sidebar/default-avatar.png";
-import chat from "./sidebar/nav-chat.svg";
-import contacts from "./sidebar/nav-contacts.svg";
-import setting from "./sidebar/nav-setting.svg";
-import logout from "./sidebar/logout.svg";
-import search from "./search.svg";
-
-import selectChat from "./chat/select-chat.svg";
-import send from "./chat/send.svg";
-import fileIcon from "./chat/clip-files.svg";
-
-export {
-  googleLogo,
-  checked,
-  notChecked,
-  verifyEmail,
-  hidePassword,
-  showPassword,
-  defaultAvatar,
-  chat,
-  contacts,
-  setting,
-  logout,
-  search,
-  selectChat,
-  send,
-  fileIcon,
-};
+export { googleLogo, verifyEmail, defaultAvatar, selectChat };
