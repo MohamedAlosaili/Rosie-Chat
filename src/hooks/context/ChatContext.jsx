c7bdae50b923e5900144c93a094c9613ab68d2f0
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-08 22:41:34 +0300

    REFACTOR remove the retrieve user info function
    I changed the structure of the chat document to store users'
    info rather than retrieving them on each open the chat
    But I have to update them when one of the ends changes his
    info like photURL or displayName

--- src/hooks/context/ChatContext.jsx
+++ src/hooks/context/ChatContext.jsx
@@ -1,46 +1,16 @@
-import React, { useContext, useEffect, useState } from "react";
-
-import { getDoc, doc } from "firebase/firestore";
+import React, { useState } from "react";
 
 import { chatDocTemplate } from "util";
-import { db } from "rosie-firebase";
-import { UserContext } from "hooks/context/UserContext";
 
 const ChatContext = React.createContext();
 
 function ChatContextProvider({ children }) {
-  const { currentUser } = useContext(UserContext);
   const [selectedChat, setSelectedChat] = useState(chatDocTemplate());
 
-  useEffect(() => {
-    if (selectedChat.id && !selectedChat.isGroup && !selectedChat.chatName) {
-      receiverInfo().then((data) => {
-        console.log(data);
-        setSelectedChat((prevSelected) => ({ ...prevSelected, ...data }));
-      });
-    }
-  }, [selectedChat]);
+  console.log("Re-rendered <ChatContext />");
 
   function changeChat(chat) {
     setSelectedChat(chat);
-  }
-
-  async function receiverInfo() {
-    if (!selectedChat.id) return;
-
-    const receiverId = selectedChat?.members.filter(
-      (memberId) => memberId !== currentUser.uid
-    )[0];
-    const receiverRef = doc(db, "users", receiverId);
-
-    try {
-      const { displayName, photoURL, ...userInfo } = (
-        await getDoc(receiverRef)
-      ).data();
-      return { chatName: displayName, chatPhotoURL: photoURL, ...userInfo };
-    } catch (error) {
-      console.log(error);
-    }
   }
 
   function emptyChat() {
