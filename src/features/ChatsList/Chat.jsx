7d3083fb47adaa65a0c513612d475941d0476057
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-08 23:01:56 +0300

    REFACTOR use the chatInfo in the document instead of retrieving them

--- src/features/ChatsList/Chat.jsx
+++ src/features/ChatsList/Chat.jsx
@@ -1,43 +1,16 @@
-import { memo, useContext, useEffect, useState } from "react";
+import { memo, useContext } from "react";
 import PropTypes from "prop-types";
 
-import { doc, getDoc } from "firebase/firestore";
+import { ChatContext } from "hooks/context";
+import { Image } from "components";
 
-import { ChatContext, UserContext } from "hooks/context";
-import { db } from "rosie-firebase";
-import { SkeletonLoader } from "components";
+const Chat = ({ chat, currentUserId, isSelected }) => {
+  const { changeChat } = useContext(ChatContext);
 
-const Chat = ({ chat, isSelected }) => {
-  const { currentUser } = useContext(UserContext);
-  const { selectedChat, changeChat } = useContext(ChatContext);
-
-  const [chatInfo, setChatInfo] = useState(() => ({
-    chatName: chat.chatName,
-    chatPhotoURL: chat.chatPhotoURL,
-  }));
-
-  useEffect(() => {
-    if (!chat.isGroup) {
-      receiverInfo().then((chatInfo) => setChatInfo(chatInfo));
-    }
-  }, []);
-
-  async function receiverInfo() {
-    const receiverId = chat.members.filter(
-      (memberId) => memberId !== currentUser.uid
-    )[0];
-    const receiverRef = doc(db, "users", receiverId);
-
-    try {
-      const userInfo = (await getDoc(receiverRef)).data();
-      return {
-        chatName: userInfo.displayName,
-        chatPhotoURL: userInfo.photoURL,
-      };
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  const receiverId = chat.members
+    .filter((memberId) => memberId !== currentUserId)
+    .join("");
+  const chatInfo = chat.isGroup ? chat.chatInfo : chat[receiverId];
 
   // TODO: This block needs some refactor
   const today = new Date();
@@ -52,28 +25,23 @@ const Chat = ({ chat, isSelected }) => {
   }
   const timeFormater = new Intl.DateTimeFormat("en-US", options);
 
-  if (!chatInfo.chatName) {
-    return <SkeletonLoader.Card isChat={true} />;
-  }
-
   return (
     <li
-      onClick={() => selectedChat.id !== chat.id && changeChat(chat)}
+      onClick={() => !isSelected && changeChat(chat)}
       className={`relative z-10 mb-2 grid cursor-pointer select-none grid-cols-[auto_1fr] gap-4 overflow-hidden rounded-xl p-4 text-sm 
                       transition-colors before:absolute before:inset-0 before:-z-10 before:opacity-50 before:transition-colors last:mb-0 
                       dark:hover:before:bg-primary-800 dark:active:before:bg-primary-700 ${
                         isSelected ? "dark:bg-primary-800" : ""
                       }`}
     >
-      <img
-        src={chatInfo.chatPhotoURL}
-        alt={`${chatInfo.chatName} ${chat.isGroup ? "group" : ""} photo`}
-        className="aspect-square w-14 rounded-full object-cover"
+      <Image
+        img={{ url: chatInfo.photoURL, name: chatInfo.name }}
+        className="aspect-square w-14 rounded-full"
       />
       <div>
         <div className="mb-2 flex items-center justify-between gap-2">
           <h3 className="truncate text-base font-medium dark:text-primary-200">
-            {chatInfo.chatName}
+            {chatInfo.name}
           </h3>
           <time className="whitespace-nowrap">
             {timeFormater.format(chat.lastMsg.createdAt?.toDate())}
@@ -82,7 +50,7 @@ const Chat = ({ chat, isSelected }) => {
         <div className="flex items-center justify-between gap-2">
           <p className="truncate">
             {chat.lastMsg.message === "Say hi to " && !chat.lastMsg.uid
-              ? chat.lastMsg.message + chatInfo.chatName + " 👋"
+              ? chat.lastMsg.message + chatInfo.name + " 👋"
               : chat.lastMsg.message}
           </p>
           {chat?.unreadMsgs > 0 && (
@@ -98,22 +66,22 @@ const Chat = ({ chat, isSelected }) => {
 
 Chat.proptypes = {
   chat: PropTypes.shape({
-    photoURL: PropTypes.string,
-    isGroup: PropTypes.bool,
-    unreadMsgs: PropTypes.number,
-    name: PropTypes.string,
-    id: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    isGroup: PropTypes.bool.isRequired,
     lastMsg: PropTypes.shape({
       uid: PropTypes.oneOf([PropTypes.string, PropTypes.symbol]),
       message: PropTypes.string,
       createdAt: PropTypes.object,
+    }),
+    ["chatInfo"]: PropTypes.shape({
+      name: PropTypes.string,
+      photoURL: PropTypes.string,
     }),
   }),
   isSelected: PropTypes.bool,
 };
 
 Chat.defaultProps = {
-  unreadMsg: 0,
   isSelected: false,
 };
 
