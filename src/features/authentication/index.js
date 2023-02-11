64cb9e5203e155d078912c134f6fde0a3d9bb25d
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-11 15:19:32 +0300

    REMOVE

--- src/features/authentication/index.js
+++
@@ -1,6 +0,0 @@
-import Authentication from "./Authentication";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
-import VerifyEmail from "./VerifyEmail";
-
-export { Authentication, SignIn, SignUp, VerifyEmail };
