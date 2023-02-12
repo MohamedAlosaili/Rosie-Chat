b235730f4f3a8c61093c65c00c66e50bfe95d521
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-12 12:44:21 +0300

    ADD EditInfoModal That handle all editing

---
+++ src/features/Profile/EditInfoModal.jsx
@@ -0,0 +1,223 @@
+import { memo, useContext, useState } from "react";
+import PropTypes from "prop-types";
+
+import { TbEdit } from "react-icons/tb";
+import { AnimatePresence } from "framer-motion";
+import {
+  collection,
+  collectionGroup,
+  getDocs,
+  query,
+  doc,
+  where,
+  writeBatch,
+} from "firebase/firestore";
+
+import useValidateFile from "hooks/useValidateFile";
+import useUploadFile from "hooks/useUploadFile";
+import useError from "hooks/useError";
+import Input from "components/Input";
+import Modal from "components/Modal";
+import Image from "components/Image";
+import StatusMessage from "components/StatusMessage";
+import { UserContext } from "context/UserContext";
+import { db } from "rosie-firebase";
+
+function EditInfoModal({ setShowEditModal }) {
+  const { currentUser } = useContext(UserContext);
+
+  const [fileUploader] = useUploadFile("photoURL");
+  const [file, changeFile, invalidFile] = useValidateFile();
+
+  const { displayName, about, photoURL } = currentUser;
+  const [editValues, setEditValues] = useState({ displayName, about });
+  const [updateUserInfoLoading, setUpdateUserInfoLoading] = useState(false);
+  const [updateUserInfoError, setUpdateUserInfoError] = useError();
+
+  async function updateUserInfo() {
+    if (editValues.displayName === "") {
+      setUpdateUserInfoError("Name can't be empty!");
+      return;
+    } else if (editValues.displayName.length > 64) {
+      setUpdateUserInfoError("Name is too long. Choose shorter one");
+      return;
+    }
+    if (
+      editValues.displayName === currentUser.displayName &&
+      editValues.about === currentUser.about &&
+      !file.validFile
+    ) {
+      setUpdateUserInfoError("Nothing change in your info");
+      return;
+    }
+
+    try {
+      const { displayName, about } = editValues;
+
+      let newValues = {};
+
+      if (displayName !== currentUser.displayName)
+        newValues.displayName = displayName;
+      if (about !== currentUser.about) newValues.about = about;
+      if (
+        displayName === currentUser.displayName &&
+        about === currentUser.about
+      )
+        newValues = null;
+
+      setUpdateUserInfoLoading(true);
+      setUpdateUserInfoError(undefined);
+
+      const batch = writeBatch(db);
+      const currentUserRef = doc(db, "users", currentUser.uid);
+
+      let photoURL;
+      if (file.validFile) {
+        photoURL = (await fileUploader(file.validFile, currentUser.uid)).url;
+        const info = newValues ? newValues : {};
+        await batch.update(currentUserRef, {
+          photoURL,
+          ...info,
+        });
+      } else if (newValues) {
+        await batch.update(currentUserRef, {
+          ...newValues,
+        });
+      }
+
+      if (newValues?.displayName || photoURL) {
+        const newDisplayName = newValues?.displayName
+          ? { name: displayName }
+          : {};
+        const newPhotoURL = photoURL ? { photoURL } : {};
+        const newUserInfo = { ...newDisplayName, ...newPhotoURL };
+
+        await updateMessagesDocs(newUserInfo, batch);
+        await updateChatsDocs(newUserInfo, batch);
+      }
+
+      batch.commit();
+      setUpdateUserInfoLoading(false);
+      setShowEditModal(false);
+    } catch (error) {
+      console.log(error);
+      setUpdateUserInfoError(typeof error === "object" ? error.message : error);
+      setUpdateUserInfoLoading(false);
+    }
+  }
+
+  async function updateMessagesDocs(newUserInfo, batch) {
+    const senderName = newUserInfo?.name
+      ? { senderName: newUserInfo.name }
+      : {};
+    const senderPhotoURL = newUserInfo?.photoURL
+      ? { senderPhotoURL: newUserInfo.photoURL }
+      : {};
+
+    const messagesRef = collectionGroup(db, "messages");
+    const messagesQuery = query(
+      messagesRef,
+      where("isGroupMessage", "==", true),
+      where("senderId", "==", currentUser.uid)
+    );
+    const messages = await getDocs(messagesQuery);
+
+    messages.forEach(async (doc) => {
+      await batch.update(doc.ref, {
+        ...senderName,
+        ...senderPhotoURL,
+      });
+    });
+  }
+
+  async function updateChatsDocs(newUserInfo, batch) {
+    const chatsQuery = query(
+      collection(db, "chats"),
+      where("members", "array-contains", currentUser.uid),
+      where("isGroup", "==", false)
+    );
+    const chats = await getDocs(chatsQuery);
+
+    chats.forEach(async (chat) => {
+      await batch.update(chat.ref, {
+        [currentUser.uid]: { ...chat.data()[currentUser.uid], ...newUserInfo },
+      });
+    });
+  }
+
+  return (
+    <>
+      <AnimatePresence>
+        {(invalidFile || updateUserInfoError) && (
+          <StatusMessage
+            type="error"
+            message={invalidFile ?? updateUserInfoError}
+          />
+        )}
+        {updateUserInfoLoading && (
+          <StatusMessage type="loading" message="Updating info..." />
+        )}
+      </AnimatePresence>
+      <Modal
+        kay="EditInfoModal"
+        closeModal={() =>
+          updateUserInfoLoading ? null : setShowEditModal(false)
+        }
+        actionButtonName={updateUserInfoLoading ? "Updating..." : "Update"}
+        actionButtonHandler={() =>
+          updateUserInfoLoading ? null : updateUserInfo()
+        }
+        className="flex min-w-[20rem] flex-col gap-4"
+      >
+        <h1 className="text-center font-semibold text-primary-200">
+          Edit Information
+        </h1>
+        <label
+          className="relative mx-auto w-28 cursor-pointer overflow-hidden rounded-full"
+          title="Change photo"
+        >
+          <input
+            type="file"
+            value={file.value}
+            disabled={updateUserInfoLoading}
+            onChange={changeFile}
+            className="hidden"
+          />
+          <Image
+            img={{ url: file.previewUrl || photoURL, name: displayName }}
+            className="aspect-square w-full"
+          />
+          {!file.previewUrl && (
+            <div className="absolute inset-0 grid place-items-center bg-black/50 transition-colors hover:bg-black/25">
+              <TbEdit size={25} className="" />
+            </div>
+          )}
+        </label>
+        <Input
+          label="Name"
+          type="text"
+          name="displayName"
+          disabled={updateUserInfoLoading}
+          value={editValues.displayName}
+          setValue={setEditValues}
+          placeholder="Display name"
+        />
+        <Input
+          label="About"
+          type="text"
+          name="about"
+          disabled={updateUserInfoLoading}
+          value={editValues.about}
+          setValue={setEditValues}
+          placeholder="About"
+        />
+      </Modal>
+    </>
+  );
+}
+
+EditInfoModal.propTypes = {
+  setShowEditModal: PropTypes.func,
+};
+
+export default memo(EditInfoModal);
