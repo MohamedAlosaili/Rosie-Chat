3d228c644a012677c7af6650e108e842cfa64003
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-13 14:58:15 +0300

    REFACTOR FileInput to receive sending hook functions
    Instead of handle sending in two separate places Form/FileInput
    I initialize useSendMessage in Form component and use it's functions
    in FileInput by passing them to FileInput component as props

--- src/features/ChatBox/Form/FileInput.jsx
+++ src/features/ChatBox/Form/FileInput.jsx
@@ -4,101 +4,80 @@ import PropTypes from "prop-types";
 import { AnimatePresence } from "framer-motion";
 import { AiOutlinePaperClip } from "react-icons/ai";
 
-import useSendMessage from "./useSendMessage";
 import useValidateFile from "hooks/useValidateFile";
 import StatusMessage from "components/StatusMessage";
-import Button from "components/Button";
 import Input from "components/Input";
 import Modal from "components/Modal";
 import Video from "components/Video";
 import Image from "components/Image";
 
-const FileInput = ({ scrollToBottom }) => {
+const FileInput = ({ message, setMessage, sendMessageHandler, sending }) => {
   const [file, changeFile, fileError, closePreview] = useValidateFile([
     "image",
     "video",
   ]);
-  const [message, setMessage, sendMessageHandler, sending, sendingError] =
-    useSendMessage("caption", scrollToBottom, closePreview);
 
   const { validFile } = file;
   return (
     <>
       <AnimatePresence>
-        {(fileError || sendingError) && (
-          <StatusMessage
-            type="error"
-            message={fileError ?? sendingError}
-            zIndex="z-[60]"
-          />
+        {fileError && (
+          <StatusMessage type="error" message={fileError} zIndex="z-[60]" />
         )}
-        {/* key="sendFileModal" to inform React that is the same component so no need to unmount on each render */}
         {validFile && (
           <Modal
             key="sendFileModal"
             closeModal={() => (sending ? null : closePreview())}
+            actionButtonName={sending ? "Sending..." : "Send"}
+            actionButtonHandler={(e) =>
+              sending ? null : sendMessageHandler(e, validFile, closePreview)
+            }
+            className="flex flex-col gap-4"
           >
-            <div className="rounded-xl p-6 dark:bg-primary-800">
-              {validFile.type.startsWith("video") ? (
-                <Video
-                  video={{ url: file.previewUrl, type: "video/mp4" }}
-                  autoPlay={true}
-                  className="mb-2 w-80 rounded-xl"
-                />
-              ) : (
-                <Image
-                  img={{ url: file.previewUrl, name: "" }}
-                  className="mb-2 aspect-square w-80 rounded-xl"
-                />
-              )}
-              <Input
-                type="text"
-                placeholder="Caption"
-                name="caption"
-                value={message.caption}
-                setValue={setMessage}
+            {validFile.type.startsWith("video") ? (
+              <Video
+                video={{ url: file.previewUrl, type: "video/mp4" }}
+                autoPlay={true}
+                className="-mb-2 w-80 max-w-full rounded-xl"
               />
-              <div className="mt-4 flex justify-between">
-                <Button
-                  disabled={sending}
-                  type="button"
-                  additionClasses="w-32"
-                  handleClick={closePreview}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  disabled={sending}
-                  type="button"
-                  additionClasses="w-32"
-                  handleClick={(e) => sendMessageHandler(e, validFile)}
-                >
-                  {sending ? "Sending..." : "Send"}
-                </Button>
-              </div>
-            </div>
+            ) : (
+              <Image
+                img={{ url: file.previewUrl, name: "" }}
+                className="-mb-2 aspect-square w-80 max-w-full rounded-xl"
+              />
+            )}
+            <Input
+              type="text"
+              placeholder="Caption"
+              name="text"
+              disabled={sending}
+              value={message.text}
+              setValue={setMessage}
+            />
           </Modal>
         )}
       </AnimatePresence>
-      <label htmlFor="file" className="cursor-pointer px-2">
+      <label className="cursor-pointer px-2">
         <AiOutlinePaperClip
           size={30}
           className="transition-colors dark:hover:text-primary-200"
         />
+        <input
+          type="file"
+          className="hidden"
+          value={file.value}
+          onChange={changeFile}
+        />
       </label>
-      <input
-        type="file"
-        id="file"
-        className="hidden"
-        value={file.value}
-        onChange={changeFile}
-      />
     </>
   );
 };
 
 FileInput.propTypes = {
-  scrollToBottom: PropTypes.func,
+  message: PropTypes.shape({ text: PropTypes.string }),
+  setMessage: PropTypes.func,
+  sendMessageHandler: PropTypes.func,
+  sending: PropTypes.bool,
 };
 
 export default memo(FileInput);
