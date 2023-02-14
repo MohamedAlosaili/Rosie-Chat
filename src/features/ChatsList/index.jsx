d5aa8706b8cea7c44ebd2f17798835e3de726165
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-14 16:55:30 +0300

    Add support for creating new chats and new groups

--- src/features/ChatsList/index.jsx
+++ src/features/ChatsList/index.jsx
@@ -1,22 +1,29 @@
-import { useContext } from "react";
+import { lazy, Suspense, useContext, useState } from "react";
 
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { collection, orderBy, query, where } from "firebase/firestore";
-import { BsExclamationCircleFill } from "react-icons/bs";
+import { BsExclamationCircleFill, BsFillPersonFill } from "react-icons/bs";
 import { TbEdit } from "react-icons/tb";
+import { HiUserGroup } from "react-icons/hi";
+import { AnimatePresence, motion } from "framer-motion";
 
+import useSearch from "hooks/useSearch";
 import Chat from "./Chat";
 import SearchForm from "components/SearchForm";
 import Tooltip from "components/Tooltip";
 import SkeletonLoader from "components/SkeletonLoader";
-import useSearch from "hooks/useSearch";
 import { db } from "rosie-firebase";
 import { UserContext } from "context/UserContext";
 import { ChatContext } from "context/ChatContext";
 
-function ChatsList() {
+const NewGroupModal = lazy(() => import("./NewGroupModal"));
+
+function ChatsList({ setTap }) {
   const { currentUser } = useContext(UserContext);
   const { selectedChat } = useContext(ChatContext);
+
+  const [showNewChatsMenu, setShowNewChatsMenu] = useState(false);
+  const [showNewGroupModal, setShowNewGroupModal] = useState(false);
 
   const chatsQuery = query(
     collection(db, "chats"),
@@ -25,6 +32,7 @@ function ChatsList() {
   );
   const [chats, userChatsLoading, userChatsError] =
     useCollectionData(chatsQuery);
+  // TODO: Not work well because the the info of the chat is different from private and group chats
   const [searchValue, setSearchValue, searchResults] = useSearch(
     "chats",
     chats,
@@ -42,10 +50,53 @@ function ChatsList() {
 
   return (
     <div className="flex flex-col gap-4">
-      <button className="absolute top-0 right-0 translate-y-2">
-        <TbEdit size={25} className="peer dark:text-primary-200" />
-        <Tooltip text="New chat" position="right" />
-      </button>
+      <div
+        className="absolute top-0 right-0 z-50 translate-y-2 cursor-pointer dark:text-primary-200"
+        onMouseLeave={() => showNewChatsMenu && setShowNewChatsMenu(false)}
+      >
+        <TbEdit
+          size={25}
+          className="peer"
+          onClick={() => setShowNewChatsMenu(true)}
+        />
+        <Tooltip text="New chat" position="left" />
+        <AnimatePresence>
+          {showNewChatsMenu && (
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
+              transition={{ type: "tween" }}
+              className="absolute top-2 right-2 z-10 flex w-max w-56 flex-col overflow-hidden rounded-xl text-sm dark:bg-primary-800"
+            >
+              <button
+                onClick={() => setTap("friends")}
+                className="flex items-center gap-2 p-4 transition-colors dark:hover:bg-primary-700/75"
+              >
+                <BsFillPersonFill size={20} /> New chat
+              </button>
+              <button
+                onClick={() => setShowNewGroupModal(true)}
+                className="flex items-center gap-2 p-4 transition-colors dark:hover:bg-primary-700/75"
+              >
+                <HiUserGroup size={20} /> New group
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <AnimatePresence>
+        {showNewGroupModal && (
+          <Suspense fallback={<div></div>}>
+            <NewGroupModal
+              setTap={setTap}
+              setShowNewGroupModal={setShowNewGroupModal}
+              currentUserId={currentUser.uid}
+              currentUserFriends={currentUser.friends}
+            />
+          </Suspense>
+        )}
+      </AnimatePresence>
       <SearchForm
         value={searchValue}
         setValue={setSearchValue}
@@ -68,15 +119,17 @@ function ChatsList() {
         (chatsElements?.length > 0 ? (
           <ul>{chatsElements}</ul>
         ) : (
-          <div className="break-all text-center dark:text-primary-200">
-            <p>
-              Results for:{" "}
-              <span className="italic dark:text-primary-400">
-                {searchValue}
-              </span>
-            </p>
-            <p>No chats found.</p>
-          </div>
+          searchValue !== "" && (
+            <div className="break-all text-center dark:text-primary-200">
+              <p>
+                Results for:{" "}
+                <span className="italic dark:text-primary-400">
+                  {searchValue}
+                </span>
+              </p>
+              <p>No chats found.</p>
+            </div>
+          )
         ))}
     </div>
   );
