0932ab2a6bbfe76f95ee001bdf0b2ed56ffa9c41
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-15 14:58:41 +0300

    Update stop propagation when clicking on Backdrop

--- src/components/Backdrop/index.jsx
+++ src/components/Backdrop/index.jsx
@@ -3,13 +3,19 @@ import { motion } from "framer-motion";
 import { opacityVariants } from "util/motionVariants";
 
 const Backdrop = ({ onClick, children }) => {
+
+  const handleBackdropClick = (e) => {
+    e.stopPropagation();
+    onClick(e);
+  };
+
   return (
     <motion.div
       variants={opacityVariants}
       initial="hidden"
       animate="visible"
       exit="hidden"
-      onClick={onClick}
+      onClick={handleBackdropClick}
       className={`fixed inset-0 z-40 flex cursor-pointer items-center justify-center bg-black/40 p-4`}
     >
       <button className="fixed top-4 right-4 text-xl">✖</button>
