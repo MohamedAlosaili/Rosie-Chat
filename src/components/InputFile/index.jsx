afe6fca9ce432590b19b1c459f77cf5a512d24a0
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-21 19:03:25 +0300

    Add InputFile component to allow user to upload files.

---
+++ src/components/InputFile/index.jsx
@@ -0,0 +1,39 @@
+import { memo } from "react";
+import PropTypes from "prop-types";
+
+import { TbEdit } from "react-icons/tb";
+import Image from "components/Image";
+
+const InputFile = ({ changeFile, className, img, loading, isPreviewUrl }) => (
+  <label
+    className={`relative mt-2 aspect-square w-20 min-w-[5rem] cursor-pointer overflow-hidden rounded-full ${
+      className ?? ""
+    }`}
+  >
+    <input
+      type="file"
+      value=""
+      disabled={loading}
+      onChange={changeFile}
+      className="hidden"
+    />
+    {img?.url && <Image img={img} className="aspect-square w-full" />}
+    {!isPreviewUrl && (
+      <div className="absolute inset-0 grid place-items-center bg-black/50 transition-colors hover:bg-black/25">
+        <TbEdit size={25} />
+      </div>
+    )}
+  </label>
+);
+
+InputFile.propTypes = {
+  changeFile: PropTypes.func,
+  loading: PropTypes.bool,
+  img: PropTypes.shape({
+    photoURL: PropTypes.string,
+    name: PropTypes.string,
+  }),
+  isPreviewUrl: PropTypes.bool,
+};
+
+export default memo(InputFile);
