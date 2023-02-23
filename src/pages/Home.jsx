492dc8f0df1e2ff7f80a7661a989ced85ce4ba8b
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-23 16:21:03 +0300

    Add ErrorBoundary component and convert Home to arrow function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
 import { memo } from "react";
 
-import { UserContextProvider } from "context/UserContext";
-import { ChatContextProvider } from "context/ChatContext";
 import SideBox from "features/SideBox";
 import ChatBox from "features/ChatBox";
+import ErrorBoundary from "./ErrorBoundary";
+import { UserContextProvider } from "context/UserContext";
+import { ChatContextProvider } from "context/ChatContext";
 
-function Home() {
-  return (
-    <div className={`absolute inset-0 overflow-hidden md:flex`}>
-      <UserContextProvider>
-        <ChatContextProvider>
-          <SideBox />
+const Home = () => (
+  <div className={`absolute inset-0 overflow-hidden md:flex`}>
+    <UserContextProvider>
+      <ChatContextProvider>
+        <SideBox />
+        <ErrorBoundary>
           <ChatBox />
-        </ChatContextProvider>
-      </UserContextProvider>
-    </div>
-  );
-}
+        </ErrorBoundary>
+      </ChatContextProvider>
+    </UserContextProvider>
+  </div>
+);
 
 export default memo(Home);
