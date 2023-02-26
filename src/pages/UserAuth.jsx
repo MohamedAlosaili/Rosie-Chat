2f8e05a6c78b174899c91fb4f891a75236bda609
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-02-26 21:21:06 +0300

    Update UserAuth page to remove VerifyEmail component and add StatusMessage for loading suspense fallback

--- src/pages/UserAuth.jsx
+++ src/pages/UserAuth.jsx
@@ -1,27 +1,22 @@
 import { useState, lazy, Suspense } from "react";
-import PropTypes from "prop-types";
 
 import { AnimatePresence } from "framer-motion";
+import StatusMessage from "../components/StatusMessage";
 
 const SignIn = lazy(() => import("features/authentication/SignIn"));
 const SignUp = lazy(() => import("features/authentication/SignUp"));
-const VerifyEmail = lazy(() => import("features/authentication/VerifyEmail"));
 
-function UserAuth({ user }) {
+function UserAuth() {
   const [selectedTap, setSelectedTap] = useState("signin");
 
   return (
     <div className="grid min-h-screen place-items-center">
       <div className="w-96 max-w-full p-4">
-        <Suspense>
+        <Suspense
+          fallback={<StatusMessage type="loading" message="loading..." />}
+        >
           <AnimatePresence initial={false} mode="wait">
-            {user ? (
-              <VerifyEmail
-                user={user}
-                selectedTap={selectedTap}
-                setSelectedTap={setSelectedTap}
-              />
-            ) : selectedTap === "signin" ? (
+            {selectedTap === "signin" ? (
               <SignIn
                 selectedTap={selectedTap}
                 setSelectedTap={setSelectedTap}
@@ -38,9 +33,5 @@ function UserAuth({ user }) {
     </div>
   );
 }
-
-UserAuth.propTypes = {
-  user: PropTypes.oneOfType([PropTypes.object, PropTypes.symbol]),
-};
 
 export default UserAuth;
