bef3e4e4c143c250be509c9d268355654d190f37
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-01 11:35:43 +0300

    Update index.js to use react-router-dom for routing and add NotFound component

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
+
+import App from "./App";
+import NotFound from "pages/NotFound";
 import ErrorBoundary from "pages/ErrorBoundary";
 
 const theme = localStorage.getItem("theme");
@@ -15,10 +19,22 @@ if (
   document.documentElement.classList.add("light");
 }
 
+const AppContainer = (
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: AppContainer,
+    errorElement: <NotFound />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
