9dec7ea65e6ffa91f934c82713ad70a98a1389cc
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-02 13:35:08 +0300

    Update firebaseConfig with environment variable imports.

--- src/rosie-firebase/index.js
+++ src/rosie-firebase/index.js
@@ -11,7 +11,15 @@ import { getStorage, ref } from "firebase/storage";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
-import firebaseConfig from "./firebaseConfig";
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
