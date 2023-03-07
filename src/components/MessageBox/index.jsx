f072717c97ea7599e6348a0f3751d1d4f69faa60
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-08 01:32:07 +0300

    Add MessageBox to handle textarea of messaging
    I changed the way of the message field to be an editable tag
    instead of textarea or input fields which gave me a lot of
    flexibility in style and work with multiple lines without
    restrict to the textarea

---
+++ src/components/MessageBox/index.jsx
@@ -0,0 +1,64 @@
+import { memo, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+
+const MessageBox = ({
+  message,
+  setMessage,
+  loading,
+  className,
+  placeholder,
+  hasInitialValue,
+}) => {
+  const textBoxRef = useRef(null);
+
+  useEffect(() => {
+    if (textBoxRef.current && hasInitialValue) {
+      textBoxRef.current.innerHTML = message.text;
+    }
+  }, []);
+
+  if (textBoxRef.current && message.text === "") {
+    textBoxRef.current.innerHTML = "";
+  }
+
+  function handlePaste(event) {
+    event.preventDefault();
+
+    const plaintext = event.clipboardData.getData("text/plain");
+
+    event.target.innerHTML = plaintext;
+    setMessage(event);
+  }
+
+  return (
+    <div className="relative flex-1">
+      <div
+        ref={textBoxRef}
+        contentEditable={!loading}
+        onInput={setMessage}
+        style={{ wordBreak: "break-word" }}
+        onPaste={handlePaste}
+        className={`no-scrollbar peer max-h-[6rem] overflow-x-hidden overflow-y-scroll whitespace-pre-wrap break-words px-4 text-primary-900 focus:outline-none dark:text-primary-200 ${className}`}
+        role="textbox"
+      ></div>
+      {message.text === "" && (
+        <span className="pointer-events-none absolute left-4 top-1/2 w-full -translate-y-1/2 text-primary-700 transition-opacity peer-focus:opacity-0 dark:text-primary-400">
+          {placeholder}
+        </span>
+      )}
+    </div>
+  );
+};
+
+MessageBox.propTypes = {
+  message: PropTypes.shape({
+    text: PropTypes.string,
+  }),
+  setMessage: PropTypes.func,
+  loading: PropTypes.bool,
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  hasInitialValue: PropTypes.bool,
+};
+
+export default memo(MessageBox);
