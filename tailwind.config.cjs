c674d722911fd616a1617a6a8f390e3e6010557f
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2023-03-08 15:23:18 +0300

    Remove loading, success, error, and borderRadius sections from tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,28 +16,11 @@ module.exports = {
           DEFAULT: "#4f46e5",
           400: "#6366f1",
         },
-        loading: {
-          light: "#0f172a",
-          dark: "#f1f5f9",
-        },
-        success: {
-          600: "#14532d",
-          DEFAULT: "#166534",
-          400: "#15803d",
-        },
-        error: {
-          600: "#7f1d1d",
-          DEFAULT: "#991b1b",
-          400: "#b91c1c",
-        },
         info: {
           600: "#0369a1",
           DEFAULT: "#0284c7",
           400: "#0ea5e9",
         },
-      },
-      borderRadius: {
-        50: "50%",
       },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
